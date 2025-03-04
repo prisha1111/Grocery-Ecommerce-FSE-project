ea6e7890fcf9bb66d7b7d2fe8b1b6dcc9e179671
Parasssk <paraskapoor4321@gmail.com>
2025-03-04 10:31:37 +0530

    Changes made in products.model.js

--- server/models/products.model.js
+++ server/models/products.model.js
@@ -1,61 +1,11 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/connectDB.js';
-import CategoryModel from './category.model.js';
-import SubCategoryModel from './subCategory.model.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../config/connectDB.js"; // Your Sequelize instance
 
-const ProductModel = sequelize.define('Product', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    image: {
-        type: DataTypes.JSON, // Storing image array in JSON format
-        defaultValue: []
-    },
-    unit: {
-        type: DataTypes.STRING,
-        defaultValue: ""
-    },
-    stock: {
-        type: DataTypes.INTEGER,
-        defaultValue: null
-    },
-    price: {
-        type: DataTypes.FLOAT,
-        defaultValue: null
-    },
-    discount: {
-        type: DataTypes.FLOAT,
-        defaultValue: null
-    },
-    description: {
-        type: DataTypes.TEXT,
-        defaultValue: ""
-    },
-    more_details: {
-        type: DataTypes.JSON, // Storing additional product details as JSON
-        defaultValue: {}
-    },
-    publish: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true
-    }
-}, {
-    timestamps: true, // Adds createdAt and updatedAt fields
-    tableName: 'products'
+const Product = sequelize.define("Product", {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    price: { type: DataTypes.FLOAT, allowNull: false },
+    stock: { type: DataTypes.INTEGER, allowNull: false }
 });
 
-// Many-to-Many Relationship with Categories
-ProductModel.belongsToMany(CategoryModel, { through: 'ProductCategories', foreignKey: 'productId' });
-CategoryModel.belongsToMany(ProductModel, { through: 'ProductCategories', foreignKey: 'categoryId' });
-
-// Many-to-Many Relationship with SubCategories
-ProductModel.belongsToMany(SubCategoryModel, { through: 'ProductSubCategories', foreignKey: 'productId' });
-SubCategoryModel.belongsToMany(ProductModel, { through: 'ProductSubCategories', foreignKey: 'subCategoryId' });
-
-export default ProductModel;
+export default Product;
